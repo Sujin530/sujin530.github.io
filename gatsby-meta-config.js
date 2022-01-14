d274273cf11a725805bbd10093a38066e7d30f6c
Sujin530 <tiamo8617@naver.com>
2022-01-14 21:02:39 +0900

    Information Revised

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,26 +1,26 @@
 module.exports = {
-  title: `zoomkoding.com`,
-  description: `줌코딩의 개발일기`,
+  title: `ddudin Devlog`,
+  description: `뚜딘의 개발일기`,
   language: `ko`, // `ko`, `en` => currently support versions for Korean and English
-  siteUrl: `https://www.zoomkoding.com`,
+  siteUrl: `https://sujin530.github.io`,
   ogImage: `/og-image.png`, // Path to your in the 'static' folder
   comments: {
     utterances: {
-      repo: `zoomkoding/zoomkoding-gatsby-blog`, // `zoomkoding/zoomkoding-gatsby-blog`,
+      repo: `sujin530/blog`, // `zoomkoding/zoomkoding-gatsby-blog`,
     },
   },
   ga: 'UA-134826755-2', // Google Analytics Tracking ID
   author: {
-    name: `정진혁`,
+    name: `김수진`,
     bio: {
       role: `개발자`,
-      description: ['사람에 가치를 두는', '능동적으로 일하는', '이로운 것을 만드는'],
-      thumbnail: 'zoomkoding.gif', // Path to the image in the 'asset' folder
+      description: ['계속 발전하는', '사람에 가치를 두는', '함께 협력하는'],
+      thumbnail: 'ddudin.gif', // Path to the image in the 'asset' folder
     },
     social: {
-      github: `https://github.com/zoomkoding`,
-      linkedIn: `https://www.linkedin.com/in/jinhyeok-jeong-800871192`,
-      email: `zoomkoding@gmail.com`,
+      github: `https://github.com/sujin530`,
+      linkedIn: `https://www.linkedin.com/in/sujin-kim-3b936b1a1/`,
+      email: `sujin.kim10530@gmail.com`,
     },
   },
 
@@ -43,21 +43,10 @@ module.exports = {
       // ========================================================
       // ========================================================
       {
-        date: '2018.04 ~',
-        activity: '개발 시작 & 블로그 시작',
+        date: '1991.05.30 ~',
+        activity: '응애응애
       },
-      {
-        date: '2018.07 ~ 2018.11',
-        activity: '인공지능 육목 알고리즘 개발 & 경진대회 우승',
-      },
-
-      {
-        date: '2019.06 ~ 2019.12',
-        activity: '알고리즘 휴학 & 코드포스 블루 달성',
-        links: {
-          post: '/how-to-algo',
-        },
-      },
+      /*
       {
         date: '2019.12 ~ 2020.06',
         activity: '스타트업 인턴십 진행 & Picky 개발',
@@ -67,41 +56,7 @@ module.exports = {
           appStore: 'https://apps.apple.com/app/picky-skincare-made-smarter/id1504197356',
         },
       },
-
-      {
-        date: '2020.07 ~ 2020.08',
-        activity: '2020 우아한테크캠프 참여',
-        links: {
-          post: '/woowa-tech-camp-final',
-          github: 'https://github.com/woowa-techcamp-2020/bmart-6',
-        },
-      },
-
-      {
-        date: '2021.01 ~ 2021. 04',
-        activity: '대학시간(대학교 수강신청 보조 웹 앱) 개발 및 운영',
-        links: {
-          post: '/college-timetable-development',
-          github: 'https://github.com/zoomkoding/college-timetable',
-          demo: 'https://www.timetable.college',
-        },
-      },
-      {
-        date: '2021.02 ~',
-        activity: '개발 블로그 스타터 개발 및 유지 보수',
-        links: {
-          post: '/gatsby-starter-zoomkoding-introduction',
-          github: 'https://github.com/zoomkoding/zoomkoding-gatsby-blog',
-          demo: 'https://www.zoomkoding.com',
-        },
-      },
-      {
-        date: '2021.04 ~',
-        activity: '만다오(프로모션 웹앱 빌더) 개발',
-        links: {
-          post: 'https://techblog.woowahan.com/2719',
-        },
-      },
+      */
     ],
 
     projects: [
@@ -122,6 +77,7 @@ module.exports = {
       },
       // ========================================================
       // ========================================================
+/*
       {
         title: '만다오(프로모션 웹앱 빌더) 개발',
         description:
@@ -144,30 +100,8 @@ module.exports = {
           appStore: 'https://apps.apple.com/app/picky-skincare-made-smarter/id1504197356',
         },
       },
-      {
-        title: '개발 블로그 테마 개발',
-        description:
-          '개발 블로그를 운영하는 기간이 조금씩 늘어나고 점점 많은 생각과 경험이 블로그에 쌓아가면서 제 이야기를 담고 있는 블로그를 직접 만들어보고 싶게 되었습니다. 그동안 여러 개발 블로그를 보면서 좋았던 부분과 불편했던 부분들을 바탕으로 레퍼런스를 참고하여 직접 블로그 테마를 만들게 되었습니다.',
-        techStack: ['gatsby', 'react'],
-        thumbnailUrl: 'blog.png',
-        links: {
-          post: '/gatsby-starter-zoomkoding-introduction',
-          github: 'https://github.com/zoomkoding/zoomkoding-gatsby-blog',
-          demo: 'https://www.zoomkoding.com',
-        },
-      },
-      {
-        title: '대학시간(대학교 수강신청 보조 웹 앱) 개발',
-        description:
-          '학생들이 예비 수강 신청 과정에서 겪게 되는 불편함을 개선하고자 예비 수강 신청을 도와줄 수 있는 웹사이트를 만들게 되었습니다. 특히 학기가 시작되기 전 빈번하게 바뀌는 과목 정보들을 주기적으로 최신화하고 수강 신청을 시작한 후에는 수강 공석(이삭)을 발견하여 이를 이메일로 알려주는 서비스를 진행했습니다. 2021년 1학기부터 운영하고 있으며 매학기 1000명이 넘는 재학생이 사용하고 있습니다.',
-        techStack: ['react', 'nodejs'],
-        thumbnailUrl: 'timetable.png',
-        links: {
-          post: '/gatsby-starter-zoomkoding-introduction',
-          github: 'https://github.com/zoomkoding/zoomkoding-gatsby-blog',
-          demo: 'https://www.zoomkoding.com',
-        },
-      },
+*/
+      
     ],
   },
 };
